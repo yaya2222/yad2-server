42c78fe463d945843556ff87a3e438aa4089af1b
yaakov goldman <gyaakov@gamil.com>
2023-04-19 18:02:18 +0300

    add router to delete user in globalRouter

--- ROUTES/globalRouter.js
+++ ROUTES/globalRouter.js
@@ -4,10 +4,15 @@ const express = require("express");
 const { validToken } = require("../middleware/auth");
 const router = express.Router();
 
-router.post("/addproduct", validToken,asynchandler(async (req, res) => {
+router.post("/add/product", validToken,asynchandler(async (req, res) => {
   const products = await globalBL.addProduct(req.user,req.body)
   res.send(products);
 }));
+
+router.delete("/users",validToken,asynchandler(async(req,res)=>{
+  const delUser=await globalBL.deleteUser(req.user)
+  res.send(delUser)
+}))
 
 
 
